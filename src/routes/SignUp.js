e97cd611455a90655775e5bdd741c6bcf97d9149
Byeonggeon Kim <byeonggeonmty@gmail.com>
2022-03-04 03:13:14 -0500

    add input validation part

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -4,17 +4,64 @@ import "../css/signup.css";
 
 const SignUp = () => {
   const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.currentTarget);
 
-    navigate("/success", {
-      state: {
-        firstName: data.get("firstName"),
-        email: data.get("email"),
-        password: data.get("password"),
-      },
+    // let text_dict = {};
+    // text_dict["firstName"] = ["First Name", "Invalid Name"];
+    // text_dict["email"] = ["Email Address", "Invalid Email"];
+    // text_dict["password"] = ["Password", "Invalid Password"];
+    const data = new FormData(e.currentTarget);
+    let validate = true;
+
+    data.forEach((value, key) => {
+      const element = document.getElementById(key);
+      console.log(element, key);
+      if (key === "firstName") {
+        if (!value) {
+          validate = false;
+          element.innerHTML = "First Name : empty";
+        } else if (!value.match(/^[a-zA-Z]+$/)) {
+          validate = false;
+          element.innerHTML = "First Name : only letters possible";
+        } else {
+          element.innerHTML = "First Name";
+        }
+      } else if (key === "email") {
+        if (!value) {
+          validate = false;
+          element.innerHTML = "Email Address : empty";
+        } else if (
+          !value.indexOf("@") ||
+          !value.indexOf(".") ||
+          value.split("@").length > 2 ||
+          value.split(".").length > 2
+        ) {
+          validate = false;
+          element.innerHTML = "Email Address : not email form";
+        } else {
+          element.innerHTML = "Email Address";
+        }
+      } else {
+        if (!value) {
+          validate = false;
+          element.innerHTML = "Password : empty";
+        } else {
+          element.innerHTML = "Password";
+        }
+      }
     });
+
+    if (validate) {
+      navigate("/success", {
+        state: {
+          firstName: data.get("firstName"),
+          email: data.get("email"),
+          password: data.get("password"),
+        },
+      });
+    }
   };
 
   return (
@@ -30,30 +77,20 @@ const SignUp = () => {
       </p>
 
       <form className="form" onSubmit={handleSubmit}>
-        <label className="label">First Name</label>
-        <input
-          className="input"
-          id="firstName"
-          name="firstName"
-          type="text"
-          required
-        />
-        <label className="label">Email Address</label>
-        <input
-          className="input"
-          id="email"
-          type="email"
-          name="email"
-          required
-        />
-        <label className="label">Password</label>
-        <input
-          className="input"
-          id="password"
-          type="password"
-          name="passwowrd"
-          required
-        />
+        <label className="label" id="firstName">
+          First Name
+        </label>
+        <input className="input" type="text" name="firstName" required />
+
+        <label className="label" id="email">
+          Email Address
+        </label>
+        <input className="input" type="email" name="email" required />
+
+        <label className="label" id="password">
+          Password
+        </label>
+        <input className="input" type="password" name="password" required />
         <button type="submit" className="button">
           Sign Up
         </button>
